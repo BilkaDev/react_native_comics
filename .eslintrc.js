e6bd7e1d42c80dd04631c19db7c56f7c7d6617f4
Marcin Biłka <98228475+BilkaDev@users.noreply.github.com>
2023-05-20 01:12:53 +0200

    Added eslint + prettier

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  extends: ['universe', 'universe/shared/typescript-analysis', 'plugin:react-hooks/recommended'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx', '*.d.ts'],
+      parserOptions: {
+        project: './tsconfig.json'
+      }
+    }
+  ],
+  ignorePatterns: [
+    "**/*.js",
+    "**/*.json",
+    "node_modules",
+    "public",
+    "dist",
+    "coverage",
+    "build"
+  ],
+  settings: {
+    'import/resolver': {
+      typescript: {} // this loads <rootdir>/tsconfig.json to ESLint
+    }
+  },
+  /* for lint-staged */
+  globals: {
+    __dirname: true
+  }
+}
