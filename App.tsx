1582f322a3e2ebaa0a3f104e8aeb610e7275d53f
Marcin Biłka <98228475+BilkaDev@users.noreply.github.com>
2023-05-20 14:07:51 +0200

    Added AppRoute

--- App.tsx
+++ App.tsx
@@ -2,7 +2,10 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { StatusBar } from 'expo-status-bar'
 
 import { AppProviders } from './appProviders/AppProviders'
+import { SingleComic } from './components/Comic/SingleComic'
 import { Dashboard } from './views/dashboard/Dashboard'
+
+import { AppRoute } from './AppRoute'
 
 const Stack = createNativeStackNavigator()
 
@@ -10,7 +13,16 @@ export default function App() {
   return (
     <AppProviders>
       <Stack.Navigator initialRouteName="Dashboard">
-        <Stack.Screen name="Dashboard" component={Dashboard} options={{ title: 'Comics' }} />
+        <Stack.Screen
+          name={AppRoute.dashboard}
+          component={Dashboard}
+          options={{ title: 'Comics' }}
+        />
+        <Stack.Screen
+          name={AppRoute.SingleComic}
+          component={SingleComic}
+          options={{ title: 'Single comic' }}
+        />
       </Stack.Navigator>
       <StatusBar style="auto" />
     </AppProviders>
